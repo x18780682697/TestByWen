1cee117de56de6de1503d9b1a9340941150c5b38
alexzhu <alexzhu@verizontal.com>
2020-08-14 15:37:41 +0800

    【add】设置状态栏颜色

--- app/src/main/java/wen/testbywen/MainActivity.java
+++ app/src/main/java/wen/testbywen/MainActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.widget.Toast;
 
 import java.io.IOException;
@@ -28,10 +29,27 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
+        // 测试启动动画
         findViewById(R.id.btn_test_launch_animation).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(v.getContext(), Main2Activity.class));
+            }
+        });
+
+        // 设置状态栏图标为白色
+        findViewById(R.id.btn_test_set_status_bar_icon_white).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setLightStatusBar(getWindow(), false);
+            }
+        });
+
+        // 设置状态栏图标为黑色
+        findViewById(R.id.btn_test_set_status_bar_icon_black).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setLightStatusBar(getWindow(), true);
             }
         });
 
@@ -45,5 +63,17 @@ public class MainActivity extends AppCompatActivity {
 ////        overridePendingTransition(R.anim.still, R.anim.exit);
 //
 //    }
+
+    public void setLightStatusBar(Window window, boolean lightStatusBar) {
+        // 设置浅色状态栏时的界面显示
+        View decor = window.getDecorView();
+        int ui = decor.getSystemUiVisibility();
+        if (lightStatusBar) {
+            ui |= View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+        } else {
+            ui &= ~View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+        }
+        decor.setSystemUiVisibility(ui);
+    }
 
 }
