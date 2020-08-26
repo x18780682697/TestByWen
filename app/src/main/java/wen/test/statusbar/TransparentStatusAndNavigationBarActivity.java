f318748ecff6c0117a10a63f8212db4e14699fb4
alexzhu <alexzhu@verizontal.com>
2020-08-26 16:24:52 +0800

    【chg】改进状态栏和导航栏全屏的方案

--- app/src/main/java/wen/test/statusbar/TransparentStatusAndNavigationBarActivity.java
+++ app/src/main/java/wen/test/statusbar/TransparentStatusAndNavigationBarActivity.java
@@ -1,14 +1,20 @@
 package wen.test.statusbar;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 
 import wen.testbywen.R;
 
 public class TransparentStatusAndNavigationBarActivity extends AppCompatActivity {
+
+    int originStatusColor, originNavigationColor;
+    int originVisibility;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,24 +27,48 @@ public class TransparentStatusAndNavigationBarActivity extends AppCompatActivity
 //        getWindow().setNavigationBarColor(blackColor);
 
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null){
+            actionBar.hide();
+        }
+
         setContentView(R.layout.activity_transparent_status_and_navigation_bar);
+        originStatusColor = getWindow().getStatusBarColor();
+        originNavigationColor = getWindow().getNavigationBarColor();
+        originVisibility = getWindow().getDecorView().getVisibility();
+
     }
 
     public void onClickShowTransparentBar(View view) {
-        changeStatusBarAndNavigationBar(true);
+        changeStatusBarAndNavigationBar(getWindow(), true);
     }
 
     public void onClickShowNormalBar(View view) {
-        changeStatusBarAndNavigationBar(false);
+        changeStatusBarAndNavigationBar(getWindow(),false);
     }
 
-    private void changeStatusBarAndNavigationBar(boolean isTransparent){
+    private void changeStatusBarAndNavigationBar(Window window, boolean isTransparent){
+        View decorView = window.getDecorView();
         if (isTransparent){
-            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            int newVisibility = decorView.getVisibility();
+            newVisibility |= View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
+            newVisibility |= View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            newVisibility |= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+            newVisibility |= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+            decorView.setSystemUiVisibility(newVisibility);
+            window.setStatusBarColor(Color.TRANSPARENT);
+            window.setNavigationBarColor(Color.TRANSPARENT);
+
         }else{
-            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+
+            decorView.setSystemUiVisibility(originVisibility);
+            window.setStatusBarColor(originStatusColor);
+            window.setNavigationBarColor(originNavigationColor);
+
         }
     }
 
