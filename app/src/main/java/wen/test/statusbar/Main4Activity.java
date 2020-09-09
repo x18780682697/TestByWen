9ca955667dc75ee91ced5924959edc656da95997
alexzhu <alexzhu@verizontal.com>
2020-09-09 14:26:51 +0800

    【chg】增加两种设置状态栏隐藏方式的关系说明

--- app/src/main/java/wen/test/statusbar/Main4Activity.java
+++ app/src/main/java/wen/test/statusbar/Main4Activity.java
@@ -1,8 +1,12 @@
 package wen.test.statusbar;
 
 import android.os.Bundle;
+
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import wen.testbywen.R;
 
@@ -19,7 +23,19 @@ public class Main4Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+
+        // 去titleBar和toolBar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null){
+            actionBar.hide();
+        }
+
         setContentView(R.layout.activity_main4);
+
+        // 设置隐藏状态栏，优先级高于setSystemUiVisibility方式
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         mOriginSystemUiState = getWindow().getDecorView().getSystemUiVisibility();
 
@@ -34,6 +50,11 @@ public class Main4Activity extends AppCompatActivity {
     }
 
     private void changeVisibility(boolean isShow){
+
+        // 此方式优先于setSystemUiVisibility方式
+        // 因此设置状态栏前需要先清除此方式设置的状态栏隐藏
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         View decorView = getWindow().getDecorView();
         int uiOptions = isShow ? mOriginSystemUiState : View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
